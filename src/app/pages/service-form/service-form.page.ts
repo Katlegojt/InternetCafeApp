109d94cbae06150c952db3ec059729cdff8d0342
Beverley Komane <andanimatoro@gmail.com>
2019-10-21 14:45:33 +0200

    sav

--- src/app/pages/service-form/service-form.page.ts
+++ src/app/pages/service-form/service-form.page.ts
@@ -13,7 +13,28 @@ export class ServiceFormPage implements OnInit {
 
   Ihours: "";
   Iprice: 0;
-  arraylist: Array <{Ihours :string , Iprice: number}>=[];
+  arraylist: Array <{Ihours :string , Iprice: number }>=[];
+
+  Printinghours: "";
+  Printingprice: 0;
+  Printinglist: Array <{ Printinghours:String,Printingprice:number}>=[];
+
+  Scannerhours:"";
+  Scannerprice: 0;
+  scannerlist: Array <{ Scannerhours:string,Scannerprice:number}>=[];
+
+  Faxhours:"";
+  Faxprice: 0;
+  Faxlist: Array <{Faxhours:string,Faxprice:number}>=[];
+
+  Emailhours:"";
+  Emailprice: 0;
+  Emaillist: Array <{Emailhours:String,Emailprice:number}>=[];
+
+  Bindinghours:"";
+  Bindingprice: 0;
+  Bindinglist: Array<{ Bindinghours:String,Bindingprice: number}>=[];
+  
   constructor(private alert:AlertController) { }
     
 //   validations_form: FormGroup;
@@ -179,9 +200,70 @@ ngOnInit() {
 
   add(){
     this.arraylist.push({Ihours: this.Ihours, Iprice: this.Iprice});
+    this.Printinglist.push({Printinghours:this.Printinghours,Printingprice:this.Printingprice});
+    this.scannerlist.push({Scannerhours:this.Scannerhours,Scannerprice:this.Scannerprice});
+    this.Faxlist.push({Faxhours:this.Faxhours,Faxprice:this.Faxprice});
+    this.Emaillist.push({Emailhours:this.Emailhours,Emailprice:this.Emailprice});
+    this.Bindinglist.push({ Bindinghours:this.Bindinghours,Bindingprice:this.Bindingprice,});
+    console.log(this.arraylist)
+    this.Ihours="";
+    this.Iprice= 0;
+    console.log(this.Printinglist)
+    this.Printinghours = "";
+    this.Printingprice = 0;
+    console.log( this.scannerlist)
+    this.Scannerhours ="";
+    this.Scannerprice = 0;
+    console.log(this.Faxlist)
+    this. Faxhours ="";
+    this.Faxprice =  0;
+    console.log( this.Emaillist)
+    this.Emailhours = "";
+    this.Emailprice = 0;
+    console.log(this.Bindinglist)
+    this.Bindinghours = "";
+    this.Bindingprice = 0;
+  }
+  add0(){
+    this.arraylist.push({Ihours: this.Ihours, Iprice: this.Iprice});
 
     console.log(this.arraylist)
     this.Ihours="";
     this.Iprice= 0;
+  }
+  add1(){
+    this.Printinglist.push({Printinghours:this.Printinghours,Printingprice:this.Printingprice});
+
+    console.log(this.Printinglist)
+    this.Printinghours = "";
+    this.Printingprice = 0;
+  }
+  add2(){
+    this.scannerlist.push({Scannerhours:this.Scannerhours,Scannerprice:this.Scannerprice});
+
+    console.log( this.scannerlist)
+    this.Scannerhours ="";
+    this.Scannerprice = 0;
+  }
+  add3(){
+    this.Faxlist.push({Faxhours:this.Faxhours,Faxprice:this.Faxprice});
+
+    console.log(this.Faxlist)
+    this. Faxhours ="";
+    this.Faxprice =  0;
+  }
+  add4(){
+    this.Emaillist.push({Emailhours:this.Emailhours,Emailprice:this.Emailprice});
+
+    console.log( this.Emaillist)
+    this.Emailhours = "";
+    this.Emailprice = 0;
+  }
+  add5(){
+    this.Bindinglist.push({ Bindinghours:this.Bindinghours,Bindingprice:this.Bindingprice,});
+
+    console.log(this.Bindinglist)
+    this.Bindinghours = "";
+    this.Bindingprice = 0;
   }
 }
