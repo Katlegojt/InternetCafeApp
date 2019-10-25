7ecc2f16e5f66dff54fa3407f439844c564fdba8
katlegojt <katlego209@gmail.com>
2019-10-25 10:29:40 -0700

    intergrate1

--- src/app/pages/add-internet-cafe/add-internet-cafe.page.ts
+++ src/app/pages/add-internet-cafe/add-internet-cafe.page.ts
@@ -167,7 +167,7 @@ name;
     this.geoService.getAGeopoints(address).subscribe(data => {console.log(data.results[0].geometry.location),
        this.latitude = data.results[0].geometry.location.lat,
        this.longitude = data.results[0].geometry.location.lng,
-       this.id = this.geoService.setALocation(this.latitude,this.longitude,address,name,phone,email,url,from,to,this.imageUrl,service) },
+       this.id = this.geoService.setALocation(this.latitude,this.longitude,address,name,phone,email,url,from,to,this.imageUrl,service)},
       );
       this.navCtrl.navigateForward('/service-list');
       } 
